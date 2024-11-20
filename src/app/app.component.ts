d3d86d3b39ab027cfe537f99171e271bbc56b573
Anujdev67 <senanuj569@gmail.com>
2024-11-20 12:59:51 +0530

    NgIf & NgFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit,OnDestroy {
   // title = 'my-angular-app';
-  x:number;
-  y:number;
-  msg:string;
+  x:any;  // for all datatypes
+  y:any;
+  msg:any;
   employee : any;
-
+  data: any;
+isLoggedIn:boolean=false;
+numArray:any[];
   constructor(){
     this.x=10;
     this.y=20;
@@ -21,8 +23,16 @@ export class AppComponent implements OnInit,OnDestroy {
     this.sayHello();
     this.employee={
       name : 'anurag gupta',
-      city: 'bhopal'
+      city: 'bhopal',
+      address : ""
     };
+    this.numArray=[1,2,3,4,5,6];
+  }
+  showAddress(){
+    this.employee.address = "85,Near napier Town ,484114";
+  }
+  loadData(){
+    this.data = "sample data";
   }
   ngOnDestroy(): void {
     throw new Error('Method not implemented.');
