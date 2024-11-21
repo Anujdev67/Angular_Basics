046ce4d90b7012ca195f91eb915d28500ef085d0
Anujdev67 <senanuj569@gmail.com>
2024-11-21 14:47:07 +0530

    todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,18 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+ 
+ import { NgFor } from '@angular/common';
+import { Component, OnDestroy, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgFor, NgIf } from '@angular/common';
+import { UserService } from './service/user.service';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { LoginHeaderComponent } from "./components/login-header/login-header.component";
+import { UserListComponent } from "./components/user-list/user-list.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgIf,NgFor],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit,OnDestroy {
-  // title = 'my-angular-app';
-  x:any;  // for all datatypes
-  y:any;
-  msg:any;
-  employee : any;
-  data: any;
-isLoggedIn:boolean=false;
-numArray:any[];
-  constructor(){
-    this.x=10;
-    this.y=20;
-    this.msg="";
-    this.sayHello();
-    this.employee={
-      name : 'anurag gupta',
-      city: 'bhopal',
-      address : ""
-    };
-    this.numArray=[1,2,3,4,5,6];
-  }
-  showAddress(){
-    this.employee.address = "85,Near napier Town ,484114";
-  }
-  loadData(){
-    this.data = "sample data";
-  }
-  ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  sayHello(){
-    this.msg="Hellp Anurag!";
-  }
+export class AppComponent{
+  
 }
